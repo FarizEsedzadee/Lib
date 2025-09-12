02499acdeea4ee0d0605e2754ca091eb94b25f69
FarizEsedzadee <fariz.esedov.99@gmail.com>
2025-09-12 09:45:12 +0400

    js admin

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -3,9 +3,7 @@ import { getAllBooks } from './service.js'
 const books = document.querySelector('#books')
 
 async function useFetch() {
-    const data = await getAllBooks();
-    console.log(data);
-    
+    const data = await getAllBooks();   
     printAllBooks(data)
 }
 useFetch()
@@ -13,13 +11,19 @@ useFetch()
 function printAllBooks(data) {
     data.forEach(item => {
         books.innerHTML += `<tr class="hover:bg-gray-100">
-                                <td class="px-4 py-2">1</td>
-                                <td class="px-4 py-2">Cinayət və Cəza</td>
-                                <td class="px-4 py-2">Dostoyevski</td>
+                                <td class="px-4 py-2">${item.id}</td>
+                                <td class="px-4 py-2">${item.book_name}</td>
+                                <td class="px-4 py-2">${item.author}</td>
+                                <td class="px-4 py-2"><img class="w-[50px] h-[60px] object-cover" src="${item.book_img}" /></td>
                                 <td class="px-4 py-2 space-x-2">
-                                    <button class="text-blue-600 hover:underline">Redaktə</button>
+                                    <button class="text-blue-600 hover:underline">Redaktə Et</button>
                                     <button class="text-red-600 hover:underline">Sil</button>
                                 </td>
                             </tr>`
     });
+}
+
+
+function addBook(data) {
+    
 }
