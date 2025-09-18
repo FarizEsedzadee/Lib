c6dac8ee6e658081285071d15459a34025a28d7d
fariz <fariz.esedov.98@gmail.com>
2025-09-18 22:21:30 +0400

    -test

--- assets/components/categoryModal.js
+++ assets/components/categoryModal.js
@@ -1,16 +1,18 @@
 export default function categoryModal() {
-    return(
-        `<!-- Modal -->
-        <div id="catalog-modal"
-            class="fixed inset-0 bg-black/50 bg-opacity-30 flex items-center justify-center z-50 hidden">
-            <div class="bg-white w-[500px] max-w-[90%] rounded-lg shadow-lg p-6 relative">
-                <button id="close-modal"
-                    class="absolute top-3 right-3 text-gray-600 hover:text-red-500 text-xl">
-                    <i class="fa-solid fa-xmark"></i>
-                </button>
-                <h2 class="text-lg font-semibold text-gray-800">Kataloq (Boş Modal)</h2>
-                <p class="text-sm text-gray-600 mt-2">Burada kataloq siyahısı olacaq.</p>
-            </div>
-        </div>`
-    )
+    return `
+    <div id="catalog-modal"
+      class="fixed inset-0 bg-black/50 flex items-center justify-center z-50 hidden">
+      
+      <div class="bg-white w-[500px] max-w-[90%] rounded-lg shadow-lg p-6 relative">
+        
+        <button id="close-modal"
+          class="absolute top-3 right-3 text-gray-600 hover:text-red-500 text-xl">
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+
+        <h2 class="text-lg font-semibold text-gray-800 mb-4">Kataloq</h2>
+        <a target="_blank" href="https://i.imgur.com/bTBnx1e.jpeg">Bidənə <span class="text-red-500 font-bold text-[24px]">MƏNƏ</span> bas</a>
+      </div>
+    </div>
+  `;
 }
